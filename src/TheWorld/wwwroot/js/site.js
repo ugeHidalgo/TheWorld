fb7e756e5bcb59d3c9068c308e789b0b83b5f78d
Uge Hidalgo <ugehidalgo@hotmail.com>
2016-11-01 18:23:47 +0100

    Added bootstrap

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,33 +1,18 @@
-﻿(function() {
-    //var ele = $('#username'); //jQuery style of var ele = document.getElementById('username');
-    //ele.text('Uge Hidalgo'); //ele.innerHTML = 'Uge Hidalgo';
-
-    //var main = $('#main');
-    //main.mouseenter(function() {
-    //    main.css('background-color', '#888');
-    //});
-
-    //main.mouseleave(function() {
-    //    main.css('background-color', '#eee');butto
-    //});
-
-    //var menuitems = $('ul.menu li a');
-    //menuitems.click(function() {
-    //    var me = this,
-    //        sender = $(this);
-    //    alert('Hello from :' + sender.text());
-    //});    
+﻿(function() {    
     var sideBarAndWrapper = $('#sidebar, #wrapper');
-    var toggleSidebarButton = $('#toggleSidebar');
+    var sidebarToggleButton = $('#sidebarToggle');
+    var sidebarToggleIcon = $('#sidebarToggle i.fa');
 
-    toggleSidebarButton.click(function () {
+    sidebarToggleButton.click(function () {
         var button = $(this);
 
         sideBarAndWrapper.toggleClass('hide-sidebar');  //añade o quita la clase indicada
-        if (sideBarAndWrapper.hasClass('hide-sidebar')) {
-            toggleSidebarButton.text('Show sidebar');
+        if (sideBarAndWrapper.hasClass('hide-sidebar')) { //si existe la clase hide-sidebar quita la left y añade right
+            sidebarToggleIcon.removeClass('fa-chevron-left');
+            sidebarToggleIcon.addClass('fa-chevron-right');
         } else {
-            toggleSidebarButton.text('Hide sidebar');
+            sidebarToggleIcon.removeClass('fa-chevron-right'); //viceversa
+            sidebarToggleIcon.addClass('fa-chevron-left');
         }
     });
 })();
