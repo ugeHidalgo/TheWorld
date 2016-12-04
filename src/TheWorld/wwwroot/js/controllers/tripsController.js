ce5a3ea9a089c08cd4384bd1e1f37a52df9eb054
Eugenio Hidalgo <ugeHidalgo@hotmail.com>
2016-12-04 17:02:47 +0100

    Added a table and a form to add new trips using angular. (This is provisional)

---
+++ src/TheWorld/wwwroot/js/controllers/tripsController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    //getting the existing module.
+    angular
+        .module('app-trips')
+        .controller('tripsController', tripsController);    
+    
+    function tripsController($location) {
+        var vm = this;
+        vm.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            created: new Date()
+        }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            vm.newTrip.created = new Date();
+            vm.trips.push(vm.newTrip);
+        };
+    }
+})();
